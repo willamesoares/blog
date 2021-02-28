70429456756ef4c494325d1ae20c71448109b775
Will Soares <willamesoares65@gmail.com>
2021-02-28 00:56:23 -0300

    Use skeleton elements for loading state in playlist page

--- src/pages/playlist.js
+++ src/pages/playlist.js
@@ -1,40 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { getArtist, getAccessToken, getPlaylist } from '../../spotify';
+import Skeleton from 'react-loading-skeleton'
 import { Link } from 'gatsby';
 
+import {
+  getAccessToken,
+  getArtist,
+  getPlaylist
+} from '../../spotify';
 import Layout from '../components/layout';
-import Spinner from '../components/loading';
 import SEO from '../components/seo';
 
-import { createUseStyles } from 'react-jss';
-
-const useStyles = createUseStyles({
-  '@global': {
-    '@keyframes animateBg': {
-      '0%': {
-        backgroundPosition: '100% 0'
-      },
-      '100%': {
-        backgroundPosition: '0 0'
-      }
-    }
-  },
-  loader: {
-    animation: 'animateBg 2s infinite linear',
-    backgroundColor: '#fff',
-    backgroundImage: 'linear-gradient(90deg, #726c6b9e, #464648a8, #726c6b9e, #464648a8)',
-    backgroundSize: '300% 100%',
-    boxShadow: '0 3px 14px #000',
-    height: '200px',
-    width: '200px',
-    zIndex: -1
-  }
-});
-
-const SongCard = ({ id, artist }) => {
+const SongCard = ({ songId, artistId, accessToken }) => {
   const [isLoadingIFrame, setIsLoadingIFrame] = useState(true);
-  const [isLoadingImage, setIsLoadingImage] = useState(true)
-  const styles = useStyles();
+  const [artist, setArtist] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const artistData = await getArtist(artistId, accessToken);
+
+      setArtist({
+        imageSrc: artistData.images[0].url
+      })
+    })();
+  }, [accessToken, artistId]);
 
   return (
     <div
@@ -49,27 +37,27 @@ const SongCard = ({ id, artist }) => {
         alignSelf: 'flex-start'
       }}
     >
-      <img
-        alt={artist.name}
-        src={artist.imageSrc}
-        style={{ width: '250px', margin: 0, maxHeight: '250px' }}
-        onLoad={() => setIsLoadingImage(false)}
-      />
-      { isLoadingIFrame && !isLoadingImage ? (
+      {!artist.imageSrc ? <Skeleton height={250} /> : (
         <div
-          className={styles.loader}
           style={{
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-            height: '3.8rem'
-          }}>
-        </div>
-      ) : null }
+            width: '250px',
+            margin: 0,
+            height: '250px',
+            backgroundImage: `url(${artist.imageSrc})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}
+        />
+      )}
+
       <iframe
         title="Spotify"
-        style={{ margin: 0, borderRadius: '0 0 .4rem .4rem' }}
-        src={`https://open.spotify.com/embed/track/${id}`}
+        style={{
+          margin: 0,
+          borderRadius: '0 0 .4rem .4rem',
+          display: isLoadingIFrame ? 'none' : 'block'
+        }}
+        src={`https://open.spotify.com/embed/track/${songId}`}
         width="250"
         height="80"
         frameBorder="0"
@@ -77,6 +65,7 @@ const SongCard = ({ id, artist }) => {
         allow="encrypted-media"
         onLoad={() => setIsLoadingIFrame(false)}>
       </iframe>
+      { isLoadingIFrame ? <Skeleton height={80} /> : null }
     </div>
   );
 };
@@ -84,39 +73,46 @@ const SongCard = ({ id, artist }) => {
 const Playlist = (props) => {
   const { data, location } = props;
   const siteTitle = data.site.siteMetadata.title;
-  const [trackList, setTrackList] = useState([]);
+  const [playlist, setPlaylist] = useState([]);
+  const [accessToken, setAccessToken] = useState('');
 
   useEffect(() => {
     (async () => {
-      const accessToken = await getAccessToken();
-      const playlistData = await getPlaylist(process.env.GATSBY_SPOTIFY_PLAYLIST_ID, accessToken);
+      const token = await getAccessToken();
+      setAccessToken(token);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!accessToken) {
+      return;
+    }
+
+    (async () => {
+      const playlistData = await getPlaylist(
+        process.env.GATSBY_SPOTIFY_PLAYLIST_ID,
+        accessToken
+      );
 
       const list = [];
       for (const track of playlistData.items) {
         const {
           track: {
             artists = [],
-            id: trackId
+            id: songId
           } = {}
         } = track;
         const artistId = (artists[0] || {}).id;
 
-        let artistData;
-        if (artistId) {
-          artistData = await getArtist(artistId, accessToken);
-        }
-
         list.push({
-          id: trackId,
-          artist: {
-            imageSrc: artistData.images[0].url
-          }
+          songId: songId,
+          artistId
         });
       }
 
-      setTrackList(list);
+      setPlaylist(list);
     })();
-  }, []);
+  }, [accessToken]);
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -139,8 +135,15 @@ const Playlist = (props) => {
           justifyContent: 'space-evenly'
         }}
       >
-        { trackList.length ? trackList.map(t => <SongCard key={t.id} {...t} />) : <Spinner /> }
+        { playlist.map(song => (
+          <SongCard
+            accessToken={accessToken}
+            key={song.songId}
+            {...song}
+          />
+        )) }
       </div>
+      { !playlist.length ? <Skeleton /> : null }
     </Layout>
   );
 }
